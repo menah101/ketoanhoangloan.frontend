af1c4d2bc1cc3684af4a27e4f8f5610e0a12e265
quy_le_van <quy_le_van@bita.jp>
2023-09-28 16:12:36 +0700

    feat(editor): add quill editor

--- src/assets/js/components/Quill.js
+++ src/assets/js/components/Quill.js
@@ -2,9 +2,10 @@ const onQuill = () => {
   const elementQuill = document.getElementById('editor')
   if (!elementQuill) return
 
+  // eslint-disable-next-line no-undef
   new Quill(elementQuill, {
-    theme: 'snow'
-  });
+    theme: 'snow',
+  })
 }
 
 export default onQuill
