5eb1ceddd8bd98b0f9b0f139057b983f9273aba9
quy_le_van <quy_le_van@bita.jp>
2023-09-28 16:41:46 +0700

    chore(editor): add options

--- src/assets/js/components/Quill.js
+++ src/assets/js/components/Quill.js
@@ -1,10 +1,37 @@
+import Quill from 'quill'
+import 'quill-paste-smart'
+// import '../../../../node_modules/quill/dist/quill.snow.css'
+
 const onQuill = () => {
   const elementQuill = document.getElementById('editor')
   if (!elementQuill) return
 
-  // eslint-disable-next-line no-undef
+  const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+    [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+    [{ direction: 'rtl' }], // text direction
+
+    [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+    ['image'],
+
+    ['clean'], // remove formatting button
+  ]
+
   new Quill(elementQuill, {
     theme: 'snow',
+    modules: {
+      toolbar: toolbarOptions,
+    },
   })
 }
 
